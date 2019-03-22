2ced80adfdb62e627da7c9954fcd0650bca84002
Tolik Dem <tolik.demianov@gmail.com>
2019-03-22 19:19:33 +0300

    rename and edit wp-config.php

--- www/wordpress/wp-config.php
+++ www/wordpress/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'myDb' );
 
 /** MySQL database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'user' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', 'test' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'db' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
@@ -77,7 +77,10 @@ $table_prefix = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true );
+
+// If WordPress is installed into a directory called "wordpress" for the domain example.com, define WP_SITEURL like this:
+define( 'WP_SITEURL', 'http://localhost:8080/wordpress' );
 
 /* That's all, stop editing! Happy publishing. */
 
