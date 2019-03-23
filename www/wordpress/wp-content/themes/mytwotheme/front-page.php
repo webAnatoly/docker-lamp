093f286006ba7b27df94d46ebffddfc9d80e79cf
Tolik Dem <tolik.demianov@gmail.com>
2019-03-23 11:32:06 +0300

    установил  TGM Plagin Activatin

--- www/wordpress/wp-content/themes/mytwotheme/front-page.php
+++ www/wordpress/wp-content/themes/mytwotheme/front-page.php
@@ -1,16 +1,4 @@
 <?php
-/**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package mytwotheme
- */
 
 get_header();
 ?>
@@ -23,7 +11,7 @@ get_header();
 		while ( have_posts() ) :
 			the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+			get_template_part( 'template-parts/content', 'home' );
 
 		endwhile; // End of the loop.
 		?>
---
+++ www/wordpress/wp-content/themes/mytwotheme/template-parts/content-home.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template part for displaying page content in front-page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package mytwotheme
+ */
+
+?>
+
+
+<?php
+	the_content();
+?>
