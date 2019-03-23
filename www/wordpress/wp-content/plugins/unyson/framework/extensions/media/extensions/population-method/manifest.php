53bed755402e908f0b4e9bd4a6f259151105fd1e
Tolik Dem <tolik.demianov@gmail.com>
2019-03-23 17:01:31 +0300

    удалил фреймвор unyson

--- www/wordpress/wp-content/plugins/unyson/framework/extensions/media/extensions/population-method/manifest.php
+++
@@ -1,7 +0,0 @@
-<?php if (!defined('FW')) die('Forbidden');
-
-$manifest['name']        = __( 'Population Methods', 'fw' );
-$manifest['description'] = '';
-$manifest['version'] = '1.0.19';
-
-$manifest['github_update'] = 'ThemeFuse/Unyson-PopulationMethods-Extension';
--- www/wordpress/wp-content/plugins/unyson/framework/extensions/media/extensions/slider/extensions/nivo-slider/static.php
+++
@@ -1,5 +0,0 @@
-<?php if ( ! defined( 'FW' ) ) {
-	die( 'Forbidden' );
-}
-
-wp_enqueue_script('jquery');
